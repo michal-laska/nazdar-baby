3c6579dd1bff84dfdf4daa6408c42728915f2f98
michal.laska <michal.laska@solarwinds.com>
2022-03-16 14:26:17 +0100

    CardProvider is no more final

--- src/main/java/com/lafi/cardgame/nazdarbaby/card/CardProvider.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/card/CardProvider.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public final class CardProvider {
+public class CardProvider {
 
 	public static final Card CARD_PLACEHOLDER = Card.createCardPlaceholder();
 
