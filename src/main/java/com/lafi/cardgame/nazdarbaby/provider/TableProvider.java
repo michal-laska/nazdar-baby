66b03c989532822cbb728884638e0d74cc87b9d8
michal.laska <michal.laska@solarwinds.com>
2022-03-22 10:13:11 +0100

    rename isCorrectPasswordHash method to verifyPasswordHash

--- src/main/java/com/lafi/cardgame/nazdarbaby/provider/TableProvider.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/provider/TableProvider.java
@@ -24,7 +24,7 @@ public class TableProvider {
 	}
 
 	public boolean isTableCreated(Table table) {
-		return table != null && !table.isCorrectPasswordHash(null);
+		return table != null && !table.verifyPasswordHash(null);
 	}
 
 	public Set<String> getTableNames() {
