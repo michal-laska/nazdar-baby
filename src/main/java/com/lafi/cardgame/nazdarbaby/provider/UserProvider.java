8535866c8c545b13702272e745b26c58dda38b09
michal.laska <michal.laska@solarwinds.com>
2022-03-24 11:46:35 +0100

    remove static creation of UserProvider

--- src/main/java/com/lafi/cardgame/nazdarbaby/provider/UserProvider.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/provider/UserProvider.java
@@ -14,22 +14,13 @@ import java.util.stream.Collectors;
 
 public final class UserProvider {
 
-	private static final Map<String, UserProvider> TABLE_NAME_TO_USER_PROVIDER = new HashMap<>();
 	private static final SessionProvider SESSION_PROVIDER = SessionProvider.INSTANCE;
 
 	private final Map<VaadinSession, User> sessionToUser = new HashMap<>();
 	private final Set<VaadinSession> loggedInSessions = new HashSet<>();
 	private final Set<String> userNames = new HashSet<>();
 
-	private UserProvider() {
-	}
-
-	static synchronized UserProvider get(String tableName) {
-		return TABLE_NAME_TO_USER_PROVIDER.computeIfAbsent(tableName, s -> new UserProvider());
-	}
-
-	void delete(String tableName) {
-		TABLE_NAME_TO_USER_PROVIDER.remove(tableName);
+	UserProvider() {
 	}
 
 	public void addUser(String userName) {
