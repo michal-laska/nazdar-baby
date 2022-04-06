c4525208a06397b003d3bd74138cf8f76828629c
michal.laska <michal.laska@solarwinds.com>
2022-04-06 10:00:51 +0200

    simple play high or low card

--- src/main/java/com/lafi/cardgame/nazdarbaby/provider/BotSimulator.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/provider/BotSimulator.java
@@ -5,6 +5,7 @@ import com.lafi.cardgame.nazdarbaby.card.CardProvider;
 import com.lafi.cardgame.nazdarbaby.card.Color;
 import com.lafi.cardgame.nazdarbaby.user.User;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -50,8 +51,9 @@ class BotSimulator {
 			return;
 		}
 
+		List<Card> activeUserCards = activeUser.getCards();
 		if (activeUser.getExpectedTakes() == null) {
-			double expectedTakes = guessExpectedTakes();
+			double expectedTakes = guessExpectedTakes(activeUserCards);
 			int expectedTakesRounded = (int) Math.round(expectedTakes);
 			if (game.isLastUserWithInvalidExpectedTakes(expectedTakesRounded)) {
 				if (expectedTakes > expectedTakesRounded) {
@@ -66,26 +68,23 @@ class BotSimulator {
 			game.afterActiveUserSetExpectedTakes();
 		} else {
 			int matchUserIndex = matchUsers.indexOf(activeUser);
-
-			List<Card> cards = activeUser.getCards();
-			Card selectedCard = selectBotCard(cards);
-
+			Card selectedCard = selectCard(activeUserCards);
 			cardPlaceholders.set(matchUserIndex, selectedCard);
 
-			int cardIndex = cards.indexOf(selectedCard);
-			cards.set(cardIndex, CardProvider.CARD_PLACEHOLDER);
+			int cardIndex = activeUserCards.indexOf(selectedCard);
+			activeUserCards.set(cardIndex, CardProvider.CARD_PLACEHOLDER);
 
 			game.changeActiveUser();
 		}
 	}
 
-	private double guessExpectedTakes() {
+	private double guessExpectedTakes(List<Card> cards) {
 		int highestCardValue = 14; //TODO do it better
 		int oneColorCardsSize = deckOfCardsSize / Color.values().length;
 		double magicNumber = highestCardValue - ((double) oneColorCardsSize / matchUsers.size()) + 1;
 
 		double guess = 0;
-		for (Card card : activeUser.getCards()) {
+		for (Card card : cards) {
 			int cardValue = card.getValue();
 			double diff = magicNumber - cardValue;
 
@@ -99,7 +98,32 @@ class BotSimulator {
 		return guess;
 	}
 
-	private Card selectBotCard(List<Card> cards) {
+	private Card selectCard(List<Card> cards) {
+		List<Card> playableCards = getPlayableCards(cards);
+		int playableCardsSize = playableCards.size();
+
+		if (playableCardsSize == 1) {
+			return playableCards.get(0);
+		}
+
+		Collections.sort(playableCards);
+
+		if (noGapsInOneColor(playableCards)) {
+			int randomIndex = random.nextInt(playableCardsSize);
+			return playableCards.get(randomIndex);
+		}
+
+		List<Card> activeUserCards = activeUser.getCards();
+		double takesGuessed = guessExpectedTakes(activeUserCards);
+		int takesNeeded = activeUser.getExpectedTakes() - activeUser.getActualTakes();
+
+		if (takesGuessed > takesNeeded) {
+			return selectLowCard(playableCards);
+		}
+		return selectHighCard(playableCards);
+	}
+
+	private List<Card> getPlayableCards(List<Card> cards) {
 		Card leadingCard = cardPlaceholders.get(0);
 		Color leadingCardColor = leadingCard.getColor();
 
@@ -114,21 +138,7 @@ class BotSimulator {
 			playableCardStream = playableCardStream.filter(card -> !card.isPlaceholder());
 		}
 
-		List<Card> playableCards = playableCardStream.toList();
-		int playableCardsSize = playableCards.size();
-
-		if (playableCardsSize == 1) {
-			return playableCards.get(0);
-		}
-
-		if (noGapsInOneColor(playableCards)) {
-			int randomIndex = random.nextInt(playableCardsSize);
-			return playableCards.get(randomIndex);
-		}
-
-		//TODO implement logic
-		int randomIndex = random.nextInt(playableCardsSize);
-		return playableCards.get(randomIndex);
+		return new ArrayList<>(playableCardStream.toList());
 	}
 
 	private void rememberCardsFromTable() {
@@ -138,18 +148,17 @@ class BotSimulator {
 		}
 	}
 
-	private boolean noGapsInOneColor(List<Card> playableCards) {
-		Card firstPlayableCard = playableCards.get(0);
-		boolean playableCardsInOneColor = playableCards.stream().allMatch(card -> card.getColor() == firstPlayableCard.getColor());
+	private boolean noGapsInOneColor(List<Card> sortedPlayableCards) {
+		Card firstPlayableCard = sortedPlayableCards.get(0);
+		boolean playableCardsInOneColor = sortedPlayableCards.stream().allMatch(card -> card.getColor() == firstPlayableCard.getColor());
 		if (!playableCardsInOneColor) {
 			return false;
 		}
 
-		Collections.sort(playableCards);
-		Card lowestPlayableCard = playableCards.get(0);
-		Card highestPlayableCard = playableCards.get(playableCards.size() - 1);
+		Card lowestPlayableCard = sortedPlayableCards.get(0);
+		Card highestPlayableCard = sortedPlayableCards.get(sortedPlayableCards.size() - 1);
 
-		int expectedSize = highestPlayableCard.getValue() - lowestPlayableCard.getValue() - playableCards.size() + 1;
+		int expectedSize = highestPlayableCard.getValue() - lowestPlayableCard.getValue() - sortedPlayableCards.size() + 1;
 
 		if (expectedSize == 0) {
 			return true;
@@ -162,5 +171,13 @@ class BotSimulator {
 				.toList();
 
 		return expectedSize == playedOutCardsInSameColor.size();
+	}
+
+	private Card selectLowCard(List<Card> sortedPlayableCards) {
+		return sortedPlayableCards.get(0);
+	}
+
+	private Card selectHighCard(List<Card> sortedPlayableCards) {
+		return sortedPlayableCards.get(sortedPlayableCards.size() - 1);
 	}
 }
