4ad52a730f11172bfb80dacc983c1a932ca65088
michal.laska <michal.laska@solarwinds.com>
2022-04-11 10:16:09 +0200

    fix card sorting

--- src/main/java/com/lafi/cardgame/nazdarbaby/provider/BotSimulator.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/provider/BotSimulator.java
@@ -8,7 +8,6 @@ import com.lafi.cardgame.nazdarbaby.user.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -219,7 +218,7 @@ class BotSimulator {
 		}
 
 		List<Card> sortedPlayableCards = playableCardStream
-				.sorted(Comparator.comparingInt(Card::getValue))
+				.sorted()
 				.toList();
 		return new ArrayList<>(sortedPlayableCards);
 	}
