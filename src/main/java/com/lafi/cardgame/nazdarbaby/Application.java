659bcb032ca37db8708714c12c4fe8a88b47e16c
michal.laska <michal.laska@solarwinds.com>
2022-05-12 13:50:40 +0200

    Progressive Web App (PWA) annotation added

--- src/main/java/com/lafi/cardgame/nazdarbaby/Application.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/Application.java
@@ -2,6 +2,7 @@ package com.lafi.cardgame.nazdarbaby;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @Push
+@PWA(name = "Nazdar baby", shortName = "Nazdar baby")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
