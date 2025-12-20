42476678780342e5d79216519d765289d2662097
Michal Laska <michal.laska@ataccama.com>
2025-12-20 18:07:47 +0100

    bump deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,13 @@ repositories {
 }
 
 dependencies {
+    val commonsLang3Version: String by project
     val commonsRngVersion: String by project
     val springBootVersion: String by project
     val vaadinVersion: String by project
 
     implementation("com.vaadin:vaadin-spring-boot-starter:$vaadinVersion")
+    implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
     implementation("org.apache.commons:commons-rng-simple:$commonsRngVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
