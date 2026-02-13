2643154da535bc700bea0e41492612cf582325fe
Michal Laska <michal.laska@ataccama.com>
2026-02-13 20:19:44 +0100

    remove unnecessary SpringBootServletInitializer

--- src/main/java/com/lafi/cardgame/nazdarbaby/Application.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/Application.java
@@ -7,12 +7,11 @@ import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Push
 @SpringBootApplication
 @StyleSheet(Lumo.STYLESHEET)
-public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+public class Application implements AppShellConfigurator {
 
 	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
