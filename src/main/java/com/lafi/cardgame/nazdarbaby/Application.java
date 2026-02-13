b4088947d73f2dd24cebfbb7ec8a81b8e481d591
Michal Laska <michal.laska@ataccama.com>
2026-02-13 20:15:35 +0100

    use @StyleSheet(Lumo.STYLESHEET)

--- src/main/java/com/lafi/cardgame/nazdarbaby/Application.java
+++ src/main/java/com/lafi/cardgame/nazdarbaby/Application.java
@@ -1,14 +1,17 @@
 package com.lafi.cardgame.nazdarbaby;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Push
 @SpringBootApplication
+@StyleSheet(Lumo.STYLESHEET)
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	static void main(String[] args) {
